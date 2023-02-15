86a7bd3fe678b73e27f02e0b888eff61fe3ab9c5
Oleg <olegashepelev@gmail.com>
2023-02-15 22:23:25 +0600

    Complete Final test

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string[] array = { "hello", "2", "world", ":-)", "guy" };
+string[] newArray = ChangeArray(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}] -> [{String.Join(", ", newArray)}]");
+
+string[] ChangeArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref newArray, count);
+    return newArray;
+}
